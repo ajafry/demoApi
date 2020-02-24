5931ab53444d24f9aee7688a0b9c4adb32aa2a3d
ajafry <ajafry@gmail.com>
2020-02-24 13:35:07 -0500

    updated with logging and tracing

--- application.py
+++ application.py
@@ -2,9 +2,25 @@ from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_restful import reqparse
 from data import testData
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 api = Api(app)
+
+logger.addHandler(AzureLogHandler(
+    connection_string='InstrumentationKey=31e9c92e-6f6c-43fe-a167-d34249265162')
+)
+
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=31e9c92e-6f6c-43fe-a167-d34249265162'),
+    sampler=ProbabilitySampler(1.0),
+)
 
 def findEmployee(lastName):
     lowercaseLastName = lastName.lower()
@@ -12,10 +28,12 @@ def findEmployee(lastName):
     for e in testData:
         if (e['LastName'].lower().startswith(lowercaseLastName)):
             matchingEmployees.append(e)
+    logger.info(f'[findEmployee] found {len(matchingEmployees)} records')
     return matchingEmployees
 
 class Employees(Resource):
     def get(self):
+        logger.info(f'[Employees] returning all data, {len(testData)} records')
         return jsonify(testData)
 
 class EmployeesByLastName(Resource):
